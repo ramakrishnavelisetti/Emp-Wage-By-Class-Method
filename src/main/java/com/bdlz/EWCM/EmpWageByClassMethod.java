d7393fb38ed6087fd9c370dbbf4ba79e6096cc57
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 18:47:04 +0530

    Added: UC1-CheckEmpPresentOrAbsent

--- src/main/java/com/bdlz/EWCM/EmpWageByClassMethod.java
+++ src/main/java/com/bdlz/EWCM/EmpWageByClassMethod.java
@@ -3,5 +3,15 @@ package com.bdlz.EWCM;
 public class EmpWageByClassMethod {
     public static void main(String[] args) {
         System.out.println("Welcome to EmpWage program by Class Method");
+        empPresentOrAbsent();
+    }
+    public static void empPresentOrAbsent() {
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is Present");
+        } else {
+            System.out.println("employee is Absent");
+        }
     }
 }
