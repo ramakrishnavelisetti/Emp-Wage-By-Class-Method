9160ac7b6bead0e4dc1ddc0cac91359920051b2d
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-01 18:40:49 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/EWCM/EmpWageByClassMethod.java
@@ -0,0 +1,7 @@
+package com.bdlz.EWCM;
+
+public class EmpWageByClassMethod {
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmpWage program by Class Method");
+    }
+}
